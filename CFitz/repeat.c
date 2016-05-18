12cb3b108e29f33db47a962d9dc749bde0b4c41e
ernestyalumni@gmail.com <ernestyalumni@gmail.com>
2016-05-17 21:03:33 -0700

    Finished repeat.c

--- CFitz/repeat.c
+++ CFitz/repeat.c
@@ -4,3 +4,18 @@
   Command Line
 
 */
+
+/*
+  Program to read and echo data from command line
+*/
+
+int main(int argc, char *argv[])
+{
+  int i;
+  
+  for (i=1; i < argc; i++) printf("%s ", argv[i]);
+  printf("\n");
+
+  return 0;
+}
+
