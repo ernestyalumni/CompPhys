1174792a0e533cb6db6dae68e25321870ec06ded
ernestyalumni <ernestyalumni@gmail.com>
2016-06-08 06:14:34 -0700

    Added helloworld.cpp, pure C++

---
+++ CUDA-By-Example/helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+ * helloworld.cpp
+ */
+/* cf. Jason Sanders, Edward Kandrot. CUDA by Example: An Introduction to General-Purpose GPU Programming 
+ * 3.2 A First Program */
+/* cf. Bjarne Stroustrup. A Tour of C++ (2013) 1.3 Hello, World! */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, World!\n";
+}
+
