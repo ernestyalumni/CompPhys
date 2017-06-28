9762f8e994c6fdd04e69dbc3305a816916e1912c
ernestyalumni <ernestyalumni@gmail.com>
2017-06-28 16:36:45 -0700

    added timer from cs344

---
+++ moreCUDA/CUSOLVER/gputimer.h
@@ -0,0 +1,48 @@
+/**
+ * @brief - In your main code, do something like this to use this: 
+ * GpuTimer timer;
+ * timer.Start();
+ * timer.Stop();
+ * printf("Time elapsed = %g ms\n", timer.Elapsed());
+ * */
+
+#ifndef __GPU_TIMER_H__
+#define __GPU_TIMER_H__
+
+struct GpuTimer
+{
+      cudaEvent_t start;
+      cudaEvent_t stop;
+ 
+      GpuTimer()
+      {
+            cudaEventCreate(&start);
+            cudaEventCreate(&stop);
+      }
+ 
+      ~GpuTimer()
+      {
+            cudaEventDestroy(start);
+            cudaEventDestroy(stop);
+      }
+ 
+      void Start()
+      {
+            cudaEventRecord(start, 0);
+      }
+ 
+      void Stop()
+      {
+            cudaEventRecord(stop, 0);
+      }
+ 
+      float Elapsed()
+      {
+            float elapsed;
+            cudaEventSynchronize(stop);
+            cudaEventElapsedTime(&elapsed, start, stop);
+            return elapsed;
+      }
+};
+
+#endif  /* __GPU_TIMER_H__ */
