a1f6c199505f609bdaf3d52032cbdf8e0611b2ae
ernestyalumni <ernestyalumni@gmail.com>
2017-09-11 04:33:40 -0700

    simpleDivergence_b.cu compiles

---
+++ moreCUDA/professional/simpleDivergence_b.cu
@@ -0,0 +1,209 @@
+/**
+ * @file   : simpleDivergence_b.cu
+ * @brief  : Simple warp divergence, with my editting
+ * @author : Ernest Yeung <ernestyalumni@gmail.com>
+ * @date   : 20170910  
+ * @ref    :  https://github.com/deeperlearning/professional-cuda-c-programming/blob/master/examples/chapter03/simpleDivergence.cu
+ * 
+ * If you find this code useful, feel free to donate directly and easily at this direct PayPal link: 
+ * 
+ * https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ernestsaveschristmas%2bpaypal%40gmail%2ecom&lc=US&item_name=ernestyalumni&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted 
+ * 
+ * which won't go through a 3rd. party such as indiegogo, kickstarter, patreon.  
+ * Otherwise, I receive emails and messages on how all my (free) material on 
+ * physics, math, and engineering have helped students with their studies, 
+ * and I know what it's like to not have money as a student, but love physics 
+ * (or math, sciences, etc.), so I am committed to keeping all my material 
+ * open-source and free, whether or not 
+ * sufficiently crowdfunded, under the open-source MIT license: 
+ * 	feel free to copy, edit, paste, make your own versions, share, use as you wish.  
+ *  Just don't be an asshole and not give credit where credit is due.  
+ * Peace out, never give up! -EY
+ * 
+ * */
+/* 
+ * COMPILATION TIP
+ * nvcc --relocatable-device-code true simpleDivergence.cu -o simpleDivergence.exe
+ * 
+ * */
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+/*
+ * simpleDivergence demonstrates divergent code on the GPU and its impact on
+ * performance and CUDA metrics.
+ */
+
+__global__ void mathKernel1(float *c)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+
+    if (tid % 2 == 0)
+    {
+        ia = 100.0f;
+    }
+    else
+    {
+        ib = 200.0f;
+    }
+
+	printf(" tid : %d, c[tid] = %.f \n ", tid, ia+ib);
+    c[tid] = ia + ib;
+}
+
+__global__ void mathKernel2(float *c)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+
+    if ((tid / warpSize) % 2 == 0)
+    {
+        ia = 100.0f;
+    }
+    else
+    {
+        ib = 200.0f;
+    }
+	printf(" tid : %d, c[tid] = %.f \n ", tid, ia+ib);
+    c[tid] = ia + ib;
+}
+
+__global__ void mathKernel3(float *c)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+
+    bool ipred = (tid % 2 == 0);
+
+    if (ipred)
+    {
+        ia = 100.0f;
+    }
+
+    if (!ipred)
+    {
+        ib = 200.0f;
+    }
+
+	printf(" tid : %d, c[tid] = %.f \n ", tid, ia+ib);
+
+    c[tid] = ia + ib;
+}
+
+__global__ void mathKernel4(float *c)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float ia, ib;
+    ia = ib = 0.0f;
+
+    int itid = tid >> 5;
+
+    if (itid & 0x01 == 0)
+    {
+        ia = 100.0f;
+    }
+    else
+    {
+        ib = 200.0f;
+    }
+
+	printf(" tid : %d, c[tid] = %.f \n ", tid, ia+ib);
+
+    c[tid] = ia + ib;
+}
+
+
+int main(int argc, char **argv)
+{
+    // set up device
+    int dev = 0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    printf("%s using Device %d: %s\n", argv[0], dev, deviceProp.name);
+
+    // set up data size
+    int size = 64;
+    int blocksize = 64;
+
+    if(argc > 1) blocksize = atoi(argv[1]);
+
+    if(argc > 2) size      = atoi(argv[2]);
+
+    printf("Data size %d ", size);
+
+    // set up execution configuration
+    dim3 block (blocksize, 1);
+    dim3 grid  ((size + block.x - 1) / block.x, 1);
+    printf("Execution Configure (block %d grid %d)\n", block.x, grid.x);
+
+    // allocate gpu memory
+    float *d_C;
+    size_t nBytes = size * sizeof(float);
+    cudaMalloc((float**)&d_C, nBytes);
+
+    // create cuda event handles
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+	float gpu_time = 0.0f;
+
+    // run a warmup kernel to remove overhead
+    cudaDeviceSynchronize();
+	cudaEventRecord(start, 0);
+    mathKernel2<<<grid, block>>>(d_C);
+    cudaDeviceSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    printf("warmup      <<< %4d %4d >>> elapsed %.10f msec \n", grid.x, block.x,
+           gpu_time );
+	cudaGetLastError();
+
+    // run kernel 1
+	cudaEventRecord(start, 0);
+    mathKernel1<<<grid, block>>>(d_C);
+	cudaDeviceSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    printf("mathKernel1 <<< %4d %4d >>> elapsed %.10f msec \n", grid.x, block.x,
+           gpu_time );
+	cudaGetLastError();
+
+    // run kernel 3
+	cudaEventRecord(start, 0);
+    mathKernel3<<<grid, block>>>(d_C);
+	cudaDeviceSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    printf("mathKernel3 <<< %4d %4d >>> elapsed %.10f msec \n", grid.x, block.x,
+           gpu_time );
+    cudaGetLastError();
+
+    // run kernel 4
+	cudaEventRecord(start, 0);
+    mathKernel4<<<grid, block>>>(d_C);
+    cudaDeviceSynchronize();
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&gpu_time, start, stop);
+    printf("mathKernel4 <<< %4d %4d >>> elapsed %.10f msec \n", grid.x, block.x,
+           gpu_time );
+	cudaGetLastError();
+
+
+    // free gpu memory and reset divece
+    cudaFree(d_C);
+    cudaDeviceReset();
+    return EXIT_SUCCESS;
+
+
+
+}
+
